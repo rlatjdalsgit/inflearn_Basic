948c16cb0ac25fc118d20fc3736f3a5aa5f79099
rlatjdalsgit <123kim003@gmail.com>
2023-08-11 16:11:53 +0900

    스프링 핵심원리 - 기본편

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -4,6 +4,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +14,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
     private final LogDemoService logDemoService;
     //나중에 구현할 서비스 객체
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")//라는 요청이 오면
     @ResponseBody//문자열 바로 반환
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();//고객이 어떤 URL로 요청했는지 알 수가 있다.
+        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
